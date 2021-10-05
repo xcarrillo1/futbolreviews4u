8c595ff2c1116cfad464cffaea645f2285182455
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-10-05 17:55:05 -0400

    stars on rating

--- models/players.js
+++ models/players.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
-    img: String,
+    img: {type: String, require: true},
     name: {type: String, required: true},
     country: {type: String, required: true},
     team: {type: String, required: true},
     position: {type: String, required: true},
     retired: {type: String, required: true},
-    rating: {type: Number, required: true},
+    rating: {type: String, required: true},
     review: {type: String, required: true},
     author: {type: String, required: true},
 });
